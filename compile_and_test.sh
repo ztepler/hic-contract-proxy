3c9593c954936fe169c483d166e767287c18ed13
Fortune 707 <fortune600music@gmail.com>
2021-05-03 00:17:22 +0300

    added sh script to compile & run tests

---
+++ compile_and_test.sh
@@ -0,0 +1,4 @@
+docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.13.0 compile-contract contracts/main/ContractProxyBigMap.ligo main > pytezos_tests/contract_proxy_bigmap.tz
+docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.13.0 compile-contract contracts/main/ContractProxyMap.ligo main > pytezos_tests/contract_proxy_map.tz
+pytest -v
+
