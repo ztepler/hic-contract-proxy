97663e9d0f9e5ee35c2057a4b4615827f7b4b194
ztepler <ztepler@gmail.com>
2021-07-10 22:28:07 +0300

    fixed test and andded more checks for registry

--- pytezos_tests/sandbox_test.py
+++ pytezos_tests/sandbox_test.py
@@ -600,16 +600,21 @@ class ContractInteractionsTestCase(SandboxedNodeTestCase):
             'subjkt': subjkt
         }
 
-        self.collab.registry(registry_params)
+        opg = self.collab.registry(registry_params).inject()
         self.bake_block()
+        result = self._find_call_result_by_hash(self.p1, opg['hash'])
+        self.assertEqual(len(result.operations), 1)
+        op = result.operations[0]
+        self.assertEqual(op['destination'], self.registry.address)
+        self.assertEqual(op['parameters']['entrypoint'], 'registry')
 
         self.assertTrue(self.registry.storage['subjkts'][subjkt])
         self.assertTrue(self.registry.storage['entries'][self.collab.address])
         self.assertEqual(
-            self.registry.storage['registries'][self.collab.address],
+            self.registry.storage['registries'][self.collab.address](),
             subjkt)
         self.assertEqual(
-            self.registry.storage['subjkts_metadata'][subjkt],
+            self.registry.storage['subjkts_metadata'][subjkt](),
             metadata)
 
 
