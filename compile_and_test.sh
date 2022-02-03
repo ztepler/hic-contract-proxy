f22738241629b857b9e8e8c877a5d9dddb3c5207
ztepler <ztepler@gmail.com>
2022-02-03 23:28:22 +0300

    added cancel swap lambda

--- compile_and_test.sh
+++ compile_and_test.sh
@@ -1,3 +1,4 @@
+docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.29.0 compile contract contracts/main/FxHashProxy.ligo -e main > build/tz/fxhash_proxy.tz
 docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.24.0 compile-contract contracts/main/HicProxy.ligo main > build/tz/hic_proxy.tz
 docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.24.0 compile-contract contracts/main/BasicProxy.ligo main > build/tz/basic_proxy.tz
 docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.24.0 compile-contract contracts/main/ContractProxyBigMap.ligo main > build/tz/contract_proxy_bigmap.tz
@@ -9,5 +10,7 @@ docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.24.0 compile-expressi
 docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.24.0 compile-expression pascaligo --init-file="contracts/lambdas/originate/hicProxy.ligo" lambda > build/tz/lambdas/originate/hic_proxy.tz
 docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.24.0 compile-expression pascaligo --init-file="contracts/lambdas/originate/basicProxy.ligo" lambda > build/tz/lambdas/originate/basic_proxy.tz
 docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.24.0 compile-contract contracts/main/MockView.ligo main > build/tz/mock_view.tz
+docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.31.0 compile expression pascaligo lambda --init-file "contracts/lambdas/call/marketplaceV3Swap.ligo" > build/tz/lambdas/call/marketplace_v3_swap.tz
+docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.31.0 compile expression pascaligo lambda --init-file "contracts/lambdas/call/marketplaceV3CancelSwap.ligo" > build/tz/lambdas/call/marketplace_v3_cancel_swap.tz
 pytest -v
 
---
+++ contracts/lambdas/call/marketplaceV3CancelSwap.ligo
@@ -0,0 +1,29 @@
+#include "../../main/HicProxy.ligo"
+
+type callParams is record [
+    marketplaceAddress : address;
+    swap_id : nat;
+]
+
+
+function lambda(const _store : storage; const packedParams : bytes) : list(operation) is
+
+block {
+
+    const unpacked = case (Bytes.unpack(packedParams) : option(callParams)) of
+    | None -> (failwith("Unpack failed") : callParams)
+    | Some(p) -> p
+    end;
+
+    const marketplace : contract(nat) =
+        case (Tezos.get_entrypoint_opt("%cancel_swap", unpacked.marketplaceAddress)
+            : option(contract(nat))) of
+        | None -> (failwith("Marketplace V3 is not found") : contract(nat))
+        | Some(con) -> con
+        end;
+
+    const callToV3 : operation = Tezos.transaction(unpacked.swap_id, 0tez, marketplace);
+
+} with list[callToV3]
+
+
