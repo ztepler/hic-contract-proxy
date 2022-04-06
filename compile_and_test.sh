025a837c52643237974ee707b26d05b9c05cecf6
ztepler <ztepler@gmail.com>
2022-04-06 15:08:34 +0300

    excluded fxhash compilation from pipeline [moved to separate branch]

--- compile_and_test.sh
+++ compile_and_test.sh
@@ -1,4 +1,3 @@
-docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.29.0 compile contract contracts/main/FxHashProxy.ligo -e main > build/tz/fxhash_proxy.tz
 docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.24.0 compile-contract contracts/main/HicProxy.ligo main > build/tz/hic_proxy.tz
 docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.24.0 compile-contract contracts/main/BasicProxy.ligo main > build/tz/basic_proxy.tz
 docker run --rm -v "$PWD":"$PWD" -w "$PWD" ligolang/ligo:0.24.0 compile-contract contracts/main/ContractProxyBigMap.ligo main > build/tz/contract_proxy_bigmap.tz
